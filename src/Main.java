3d2853fc513ac13cf54ea7bae00e2020a689564a
davidlop <davidloperahenao@gmail.com>
2024-07-26 19:11:25 -0500

    First UCO bet version

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,6 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
         final int INGRESAR_JUGADOR = 0;
